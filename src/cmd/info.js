058bfdffbe302617251fd72354218b137684f4d2
Thomas Ortiz <thomasortiz95@gmail.com>
2018-03-18 20:04:46 -0400

    Added changes to info to see more bot information

--- src/cmd/info.js
+++ src/cmd/info.js
@@ -1,5 +1,15 @@
+const { version } = require('discord.js');
+
 exports.run = (bot, msg) => {
-    msg.channel.send('= PUBG Bot Information =\n\nOwner:: Thomas Ortiz\nGithub:: https://github.com/Tdortiz/PUBG-Discord-Bot\nFramework:: discord.js 11.3.0\n', { code: 'asciidoc'});
+    msg.channel.send(`= PUBG Bot Information =
+• Owner       :: Thomas Ortiz
+• Github      :: https://github.com/Tdortiz/PUBG-Discord-Bot
+• Mem Usage   :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Users       :: ${bot.users.size.toLocaleString()}
+• Servers     :: ${bot.guilds.size.toLocaleString()}
+• Channels    :: ${bot.channels.size.toLocaleString()}
+• Discord.js  :: v${version}
+• Node        :: ${process.version}`, { code: 'asciidoc'});
 };
 
 exports.conf = {
