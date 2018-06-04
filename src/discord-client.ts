d345eae2ecd4d6783b100134cbc63510400448e2
Thomas Ortiz <thomasortiz95@gmail.com>
2018-06-04 00:48:49 -0400

    debug

--- src/discord-client.ts
+++ src/discord-client.ts
@@ -4,4 +4,7 @@ import * as Discord from 'discord.js';
 export class DiscordClientWrapper extends Discord.Client {
     aliases: any;
     commands: any;
+    channel: any;
+    elevation(arg0: any): any {};
+    reload: (command: any) => Promise<{}>;
 }
