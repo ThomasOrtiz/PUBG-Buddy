f95a9f402b3b906064a4fa8c5fef593012ce0a5f
Thomas Ortiz <tdortiz@ncsu.edu>
2018-11-11 20:07:15 -0500

    Removed deprecated table migrations

--- migrations/1-setup-database-defults.sql
+++ migrations/1-setup-database-defults.sql
@@ -1,12 +1,15 @@
-/** Setup players, servers, server_registery tables */
+/**
+    Setup players, servers, server_registery, user_registery tables
+*/
+
 BEGIN;
     CREATE TABLE IF NOT EXISTS players (id SERIAL PRIMARY KEY, pubg_id TEXT, username TEXT);
 
     CREATE TABLE IF NOT EXISTS servers
         (id SERIAL PRIMARY KEY,
-        server_id TEXT,
+        server_id TEXT UNIQUE,
         default_bot_prefix TEXT DEFAULT '!pubg-',
-        default_season TEXT DEFAULT '2018-08',
+        default_season TEXT DEFAULT 'pc-2018-01',
         default_region TEXT DEFAULT 'PC_NA',
         default_mode TEXT DEFAULT 'SQUAD_FPP');
 
@@ -19,113 +22,4 @@ BEGIN;
         (id SERIAL PRIMARY KEY,
         discord_id TEXT,
         fk_players_id integer REFERENCES players (id) ON DELETE CASCADE);
-COMMIT;
-
-/** Setup seasons table */
-BEGIN;
-
-    CREATE TABLE IF NOT EXISTS seasons (id SERIAL PRIMARY KEY, name Text unique, season TEXT unique);
-
-    INSERT INTO seasons (name, season)
-    VALUES ('Season 1', '2018-01')
-    ON CONFLICT (season) do nothing;
-
-    INSERT INTO seasons (name, season)
-    VALUES ('Season 2', '2018-02')
-    ON CONFLICT (season) do nothing;
-
-    INSERT INTO seasons (name, season)
-    VALUES ('Season 3', '2018-03')
-    ON CONFLICT (season) do nothing;
-
-    INSERT INTO seasons (name, season)
-    VALUES ('Season 4', '2018-04')
-    ON CONFLICT (season) do nothing;
-
-    INSERT INTO seasons (name, season)
-    VALUES ('Season 5', '2018-05')
-    ON CONFLICT (season) do nothing;
-
-    INSERT INTO seasons (name, season)
-    VALUES ('Season 6', '2018-06')
-    ON CONFLICT (season) do nothing;
-
-    INSERT INTO seasons (name, season)
-    VALUES ('Season 7', '2018-07')
-    ON CONFLICT (season) do nothing;
-
-    INSERT INTO seasons (name, season)
-    VALUES ('Season 8', '2018-08')
-    ON CONFLICT (season) do nothing;
-COMMIT;
-
-/** Setup squad_sizes table */
-BEGIN;
-    CREATE TABLE IF NOT EXISTS squad_sizes (id SERIAL PRIMARY KEY, name TEXT unique, size integer unique);
-
-    INSERT INTO squad_sizes (name, size)
-    VALUES ('Solo', 1)
-    ON CONFLICT (size) do nothing;
-
-    INSERT INTO squad_sizes (name, size)
-    VALUES ('Duo', 2)
-    ON CONFLICT (size) do nothing;
-
-    INSERT INTO squad_sizes (name, size)
-    VALUES ('Squad', 4)
-    ON CONFLICT (size) do nothing;
-COMMIT;
-
-/** Setup modes table */
-BEGIN;
-    CREATE TABLE IF NOT EXISTS modes (id SERIAL PRIMARY KEY, fullname TEXT unique, shortname text unique);
-
-    INSERT INTO modes (fullname, shortname)
-    VALUES ('First Person Perspective', 'fpp')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO modes (fullname, shortname)
-    VALUES ('Third Person Perspective', 'tpp')
-    ON CONFLICT (fullname) do nothing;
-COMMIT;
-
-/** Setup regions table */
-BEGIN;
-    CREATE TABLE IF NOT EXISTS regions (id SERIAL PRIMARY KEY, fullname TEXT unique, shortname text unique);
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('North America', 'na')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('Asia', 'as')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('Korea', 'kr')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('Japan', 'jp')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('Korean', 'kakao')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('South America', 'sa')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('Europe', 'eu')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('Oceania', 'oc')
-    ON CONFLICT (fullname) do nothing;
-
-    INSERT INTO regions (fullname, shortname)
-    VALUES ('South-East Asia', 'sea')
-    ON CONFLICT (fullname) do nothing;
 COMMIT;
