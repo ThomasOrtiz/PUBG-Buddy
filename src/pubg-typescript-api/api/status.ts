75c7639c32e310abb8edb00b769114cd61359b0d
Thomas Ortiz <tdortiz@ncsu.edu>
2018-11-16 01:56:27 -0500

    Updated pubg api > status to use the correct endpoint

--- src/pubg-typescript-api/api/status.ts
+++ src/pubg-typescript-api/api/status.ts
@@ -1,14 +1,19 @@
-import { AxiosPromise } from 'axios';
+import { AxiosPromise } from "axios";
 
-import { IStatus } from '..';
+import { IStatus } from "..";
 
-import { PubgAPIEndpoint } from './base';
+import axios, { AxiosInstance } from 'axios';
 
+export class StatusPubgAPI {
+    private _axios: AxiosInstance;
 
-export class StatusPubgAPI extends PubgAPIEndpoint {
+    constructor() {
+        this._axios = axios.create({
+            baseURL: `https://api.playbattlegrounds.com/`,
+        });
+    }
 
-  get(): AxiosPromise<IStatus> {
-    return this.api.axios.get(`/status`);
-  }
-
+    get(): AxiosPromise<IStatus> {
+        return this._axios.get(`/status`);
+    }
 }
